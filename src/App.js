ae6ae746ee5d6cc5fa8f96060cf379358444290c
jc064522 <jc064522@gmail.com>
2018-04-26 13:30:08 +0100

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import moment from 'moment'
+import moment from "moment";
 import "./App.css";
 import { Grid, Segment, List, Accordion, Form, Label } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
@@ -9,65 +9,75 @@ import TrackerSummary from "./TrackerSummary";
 const dummyTrackers = [
   {
     name: "FANTASTIC_PIPELINE_ALL_CAPS_FOR_SOME_REASON",
-    trackerMs: moment().subtract(4, 'hours').toISOString(),
+    trackerMs: moment()
+      .subtract(4, "hours")
+      .toISOString(),
     trackerPercentage: 76,
-    lastPollAge:6.1,
-    completed:false,
-    enabled:true,
-    priority:5
+    lastPollAge: 6.1,
+    completed: false,
+    enabled: true,
+    priority: 5
   },
   {
     name: "FANTASTIC_PIPELINE_2",
-    trackerMs: moment().subtract(3, 'days').toISOString(),
+    trackerMs: moment()
+      .subtract(3, "days")
+      .toISOString(),
     trackerPercentage: 1,
-    lastPollAge:1,
-    completed:false,
-    enabled:true,
-    priority:10
+    lastPollAge: 1,
+    completed: false,
+    enabled: true,
+    priority: 10
   },
   {
     name: "FANTASTIC_PIPELINE_3",
-    trackerMs: moment().subtract(18, 'hours').toISOString(),
+    trackerMs: moment()
+      .subtract(18, "hours")
+      .toISOString(),
     trackerPercentage: 100,
-    lastPollAge:300,
-    completed:true,
-    enabled:false,
-    priority:10
+    lastPollAge: 300,
+    completed: true,
+    enabled: false,
+    priority: 10
   }
 ];
 
 const sortOptions = [
   { key: "trackerMs", value: "trackerMs", text: "Created tasks up to" },
-  { key: "trackerPercentage", value: "trackerPercentage", text: "Percentage complete" },
-  { key: "lastPollAge", value: "lastPollAge", text: "Last polled" },
+  {
+    key: "trackerPercentage",
+    value: "trackerPercentage",
+    text: "Percentage complete"
+  },
+  { key: "lastPollAge", value: "lastPollAge", text: "Last polled" }
 ];
 
 const sortDirectionOptions = [
   { key: "asc", value: "asc", text: "Ascending" },
-  { key: "desc", value: "desc", text: "Descending" },
+  { key: "desc", value: "desc", text: "Descending" }
 ];
-
 
 class App extends Component {
   // Set up some defaults
-  state = { showCompleted: false, orderBy: 'trackerMs', sortDirection:'desc'}
+  state = { showCompleted: false, orderBy: "trackerMs", sortDirection: "desc" };
 
   handleShowCompletedToggle = (e, toggleProps) => {
-    this.setState({showCompleted: toggleProps.checked})
-  }
+    this.setState({ showCompleted: toggleProps.checked });
+  };
 
   handleSortChange = (e, orderDropdownProps) => {
-    this.setState({orderBy: orderDropdownProps.value})
-  }
+    this.setState({ orderBy: orderDropdownProps.value });
+  };
 
   handleSortDirectionChange = (e, sortDirectionDropdownProps) => {
-    this.setState({sortDirection: sortDirectionDropdownProps.value})
-  }
+    this.setState({ sortDirection: sortDirectionDropdownProps.value });
+  };
 
   render() {
     const trackers = dummyTrackers;
-    const { showCompleted, orderBy, sortDirection } = this.state
-    return <div className="App">
+    const { showCompleted, orderBy, sortDirection } = this.state;
+    return (
+      <div className="App">
         <Grid>
           <Grid.Column width={16}>
             <header className="App-header">
@@ -78,9 +88,30 @@ class App extends Component {
           <Grid.Column width={8}>
             <Form>
               <Form.Group inline>
-              <Form.Checkbox inline label="Show completed?" toggle onChange={this.handleShowCompletedToggle} />
-              <Form.Dropdown inline compact label="Sort by" labeled options={sortOptions} value={orderBy} onChange={this.handleSortChange}/>
-              <Form.Dropdown inline compact label="Sort direction" labeled options={sortDirectionOptions} value={sortDirection} onChange={this.handleSortDirectionChange}/>
+                <Form.Checkbox
+                  inline
+                  label="Show completed?"
+                  toggle
+                  onChange={this.handleShowCompletedToggle}
+                />
+                <Form.Dropdown
+                  inline
+                  compact
+                  label="Sort by"
+                  labeled
+                  options={sortOptions}
+                  value={orderBy}
+                  onChange={this.handleSortChange}
+                />
+                <Form.Dropdown
+                  inline
+                  compact
+                  label="Sort direction"
+                  labeled
+                  options={sortDirectionOptions}
+                  value={sortDirection}
+                  onChange={this.handleSortDirectionChange}
+                />
               </Form.Group>
             </Form>
           </Grid.Column>
@@ -88,30 +119,32 @@ class App extends Component {
           <Grid.Column width={16}>
             <Segment.Group className="trackers-container" size="mini">
               {trackers
-                .filter(tracker => tracker.completed === showCompleted || !tracker.completed)
+                .filter(
+                  tracker =>
+                    tracker.completed === showCompleted || !tracker.completed
+                )
                 .sort((tracker1, tracker2) => {
-                  if(orderBy === 'trackerMs'){
-                    if(sortDirection === 'asc'){
-                      return tracker1.trackerMs > tracker2.trackerMs
+                  if (orderBy === "trackerMs") {
+                    if (sortDirection === "asc") {
+                      return tracker1.trackerMs > tracker2.trackerMs;
+                    } else {
+                      return tracker1.trackerMs < tracker2.trackerMs;
                     }
-                    else {
-                      return tracker1.trackerMs < tracker2.trackerMs
+                  } else if (orderBy === "trackerPercentage") {
+                    if (sortDirection === "asc") {
+                      return (
+                        tracker1.trackerPercentage > tracker2.trackerPercentage
+                      );
+                    } else {
+                      return (
+                        tracker1.trackerPercentage < tracker2.trackerPercentage
+                      );
                     }
-                  }
-                  else if(orderBy === 'trackerPercentage'){
-                    if(sortDirection === 'asc'){
-                      return tracker1.trackerPercentage > tracker2.trackerPercentage
-                    }
-                    else {
-                      return tracker1.trackerPercentage < tracker2.trackerPercentage
-                    }
-                  }
-                  else if(orderBy === 'lastPollAge'){
-                    if(sortDirection === 'asc'){
-                      return tracker1.lastPollAge > tracker2.lastPollAge
-                    }
-                    else {
-                      return tracker1.lastPollAge < tracker2.lastPollAge
+                  } else if (orderBy === "lastPollAge") {
+                    if (sortDirection === "asc") {
+                      return tracker1.lastPollAge > tracker2.lastPollAge;
+                    } else {
+                      return tracker1.lastPollAge < tracker2.lastPollAge;
                     }
                   }
                 })
@@ -121,7 +154,8 @@ class App extends Component {
             </Segment.Group>
           </Grid.Column>
         </Grid>
-      </div>;
+      </div>
+    );
   }
 }
 
