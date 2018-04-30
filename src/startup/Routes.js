211b8ef3d92076c90ebe5b89222d352abdc026b1
jc064522 <jc064522@gmail.com>
2018-04-30 14:38:06 +0100

    Add config generation and auth (WiP)

--- src/startup/Routes.js
+++ src/startup/Routes.js
@@ -14,23 +14,88 @@
  * limitations under the License.
  */
 
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-
+import React, { Component } from 'react'
+import { BrowserRouter, Route, withRouter } from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 import Home from 'sections/Home'
 import OriginalList from 'prototypes/OriginalList'
 import Graph from 'prototypes/Graph'
 import TrackerDashboard from 'sections/TrackerDashboard'
+import { AuthenticationRequest, HandleAuthenticationResponse } from 'stroom-js'
 
-const Routes = () => (
-  <BrowserRouter>
-    <div>
-      <Route exact path='/' component={Home} />
-      <Route path='/trackers' component={TrackerDashboard} />
-      <Route path='/prototypes/original_list' component={OriginalList} />
-      <Route path='/prototypes/graph' component={Graph} />
-    </div>
-  </BrowserRouter>
-)
+class Routes extends Component {
+  isLoggedIn () {
+    return !!this.props.idToken
+  }
 
-export default Routes
+  render () {
+    return (
+      <BrowserRouter>
+        <div>
+          {/* Application routes - no authentication required*/ }
+          <Route exact path='/' component={Home} />
+          {/* <Route path='/trackers' component={TrackerDashboard} /> */}
+          <Route path='/prototypes/original_list' component={OriginalList} />
+          <Route path='/prototypes/graph' component={Graph} />
+
+
+          {/* Application Routes - require authentication */}
+          <Route exact path={'/trackers'} render={() => (
+            this.isLoggedIn() ? <TrackerDashboard /> : <AuthenticationRequest
+              referrer='/'
+              uiUrl={this.props.advertisedUrl}
+              appClientId={this.props.appClientId}
+              authenticationServiceUrl={this.props.authenticationServiceUrl} />
+          )} />
+
+          {/* Authentication routes */}
+          <Route exact path={'/handleAuthentication'} render={() => (<HandleAuthenticationResponse
+            authenticationServiceUrl={this.props.authenticationServiceUrl}
+            authorisationServiceUrl={this.props.authorisationServiceUrl} />
+          )} />
+          <Route exact path={'/handleAuthenticationResponse'} render={() => (<HandleAuthenticationResponse
+            authenticationServiceUrl={this.props.authenticationServiceUrl}
+            authorisationServiceUrl={this.props.authorisationServiceUrl} />
+          )} />
+
+
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+
+// App.contextTypes = {
+//   store: PropTypes.object,
+//   router: PropTypes.shape({
+//     history: object.isRequired
+//   })
+// }
+
+// App.propTypes = {
+//   idToken: PropTypes.string.isRequired,
+//   showUnauthorizedDialog: PropTypes.bool.isRequired
+// }
+
+const mapStateToProps = state => ({
+  idToken: state.authentication.idToken,
+  // showUnauthorizedDialog: state.login.showUnauthorizedDialog,
+  advertisedUrl: state.config.advertisedUrl,
+  appClientId: state.config.appClientId,
+  authenticationServiceUrl: state.config.authenticationServiceUrl,
+  authorisationServiceUrl: state.config.authorisationServiceUrl
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  // handleSessionTimeout
+}, dispatch)
+
+export default withRouter(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Routes))
+
+
+// export default Routes
