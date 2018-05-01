2e05dba79d3df6c7fe983b53dd7a4393482a40cd
jc064522 <jc064522@gmail.com>
2018-05-01 09:21:44 +0100

    Add glorious storybooks

--- src/components/NumericInput/NumericInput.stories.js
+++ src/components/NumericInput/NumericInput.stories.js
@@ -14,52 +14,50 @@
  * limitations under the License.
  */
 
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import NumericInput from "./NumericInput";
-import { Input } from "semantic-ui-react";
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import NumericInput from './NumericInput'
 
-import "./NumericInput.css";
+import './NumericInput.css'
 
-storiesOf("NumericInput", module)
-  .add("basic", () => (
-    <div className="container">
+storiesOf('NumericInput', module)
+  .add('basic', () => (
+    <div className='container'>
       <NumericInput />
     </div>
   ))
-  .add("with default value", () => (
-    <div className="container">
-      <NumericInput defaultValue={10}/>
+  .add('with default value', () => (
+    <div className='container'>
+      <NumericInput defaultValue={10} />
     </div>
   ))
-  .add("with placeholder value", () => (
-    <div className="container">
-      <NumericInput placeholder={11}/>
+  .add('with placeholder value', () => (
+    <div className='container'>
+      <NumericInput placeholder={11} />
     </div>
   ))
-  .add("with max value of 4", () => (
-    <div className="container">
-      <NumericInput max={4} value={2}/>
+  .add('with max value of 4', () => (
+    <div className='container'>
+      <NumericInput max={4} value={2} />
     </div>
   ))
-  .add("with min value of -1", () => (
-    <div className="container">
-      <NumericInput min={-1} value={0}/>
+  .add('with min value of -1', () => (
+    <div className='container'>
+      <NumericInput min={-1} value={0} />
     </div>
   ))
-  .add("with min value of 42 and a max value of 46", () => (
-    <div className="container">
-      <NumericInput min={42} max={46} value={44}/>
+  .add('with min value of 42 and a max value of 46', () => (
+    <div className='container'>
+      <NumericInput min={42} max={46} value={44} />
     </div>
   ))
-  .add("with value lower than min", () => (
-    <div className="container">
-      <NumericInput min={4} value={2}/>
+  .add('with value lower than min', () => (
+    <div className='container'>
+      <NumericInput min={4} value={2} />
     </div>
   ))
-  .add("with value high than max", () => (
-    <div className="container">
-      <NumericInput max={4} value={6}/>
+  .add('with value high than max', () => (
+    <div className='container'>
+      <NumericInput max={4} value={6} />
     </div>
   ))
