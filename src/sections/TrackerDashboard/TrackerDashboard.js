16dcbfc0396a3b08725b3e73a8d7930c1e90daf0
jc064522 <jc064522@gmail.com>
2018-05-10 16:31:43 +0100

    Make TrackerDetails its own component (WiP)

--- src/sections/TrackerDashboard/TrackerDashboard.js
+++ src/sections/TrackerDashboard/TrackerDashboard.js
@@ -27,6 +27,7 @@ import 'semantic-ui-css/semantic.min.css';
 
 import { fetchTrackers, updateSort, sortByOptions, directions, enableToggle, updateTrackerSelection } from './trackerDashboardData';
 
+import TrackerDetails from './TrackerDetails'
 import './TrackerDashboard.css';
 
 class TrackerDashboard extends Component {
@@ -68,12 +69,12 @@ class TrackerDashboard extends Component {
     const showDetails = selectedTracker !== undefined
 
     // Set up hotkeys because anything else would be nasty
-    if(showDetails){
-      Mousetrap.bind('esc', () => onHandleTrackerSelection(undefined));
-    }
-    else{
-      Mousetrap.unbind('esc');
-    }
+    // if(showDetails){
+    //   Mousetrap.bind('esc', () => onHandleTrackerSelection(undefined));
+    // }
+    // else{
+    //   Mousetrap.unbind('esc');
+    // }
 
     return (
       <div className="App">
@@ -100,88 +101,6 @@ class TrackerDashboard extends Component {
           <Grid.Column width={4} /> */}
 
           <Grid.Column width={16}>
-            <Dimmer active={dimTable} inverted>
-              <Loader size="medium" />
-            </Dimmer>
-
-            <Dimmer.Dimmable blurring dimmed={showDetails}>
-              <Dimmer inverted active={showDetails} onClickOutside={() => onHandleTrackerSelection(undefined)}>
-                {selectedTracker ? (
-                  <div className='details-container'>
-                  <Card.Group centered className='details-content'>
-                  <Card fluid>
-                    <Card.Content>
-                      {/* <Image floated='right' size='mini' src='/assets/images/avatar/large/steve.jpg' /> */}
-                      <Card.Header>
-                        <Table className="tracker-details-table">
-                        <Table.Body>
-                      <Table.Row className="tracker-row"  >
-                              <Table.Cell className="name-column" textAlign="right" width={7}>
-                                {selectedTracker.pipelineName}
-                              </Table.Cell>
-                              <Table.Cell className="priority-column" textAlign="center" width={1}>
-                                <Label circular color="green">
-                                  {selectedTracker.priority}
-                                </Label>
-                              </Table.Cell>
-                              <Table.Cell className="progress-column" width={8}>
-                                <Progress percent={selectedTracker.trackerPercent} indicating />
-                              </Table.Cell>
-                            </Table.Row>
-                            </Table.Body>
-                            </Table>
-                      </Card.Header>
-
-
-                      <Card.Meta>
-                        This tracker:
-                      </Card.Meta>
-                      <Card.Description>
-                        <Grid centered divided columns={3}>
-                          <Grid.Column textAlign='left'>
-                            <List bulleted>
-                              <List.Item>has a <strong>last poll age</strong> of {selectedTracker.lastPollAge}</List.Item>
-                              <List.Item>has a <strong>task count</strong> of {selectedTracker.taskCount}</List.Item>
-                              <List.Item>was <strong>last active</strong> {moment(selectedTracker.trackerMs).calendar().toLowerCase()}</List.Item>
-                            </List>
-                          </Grid.Column>
-                          <Grid.Column>
-                            <List bulleted>
-                              <List.Item>{selectedTracker.status ? 'has a' : 'does not have a'} <strong>status</strong>{selectedTracker.status ? ' of ' + selectedTracker.status : undefined}</List.Item>
-                              <List.Item>
-                              {selectedTracker.streamCount ? 'has a' : 'does not have a'} <strong>stream count</strong>{selectedTracker.streamCount ? ' of ' + selectedTracker.streamCount : undefined}</List.Item>
-                              <List.Item>{selectedTracker.eventCount ? 'has an' : 'does not have an'} <strong>event count</strong>{selectedTracker.eventCount ? ' of ' + selectedTracker.eventCount : undefined}</List.Item>
-                            </List>
-
-                          </Grid.Column>
-                          <Grid.Column textAlign='left'>
-                            <List bulleted>
-                              <List.Item>was <strong>created</strong> by '{selectedTracker.createUser}' {moment(selectedTracker.createdOn).calendar().toLowerCase()}</List.Item>
-                              <List.Item>was <strong>updated</strong> by '{selectedTracker.updateUser}' {moment(selectedTracker.updatedOn).calendar().toLowerCase()}</List.Item>
-                            </List> 
-                          </Grid.Column>
-                        </Grid>
-                      </Card.Description>
-                    </Card.Content>
-                  <Card.Content extra>
-                    <Grid>
-                      <Grid.Column width={8}>
-                    <Checkbox toggle label='Enabled?' checked={selectedTracker.enabled} onMouseDown={() => onHandleEnableToggle(selectedTracker.filterId, selectedTracker.enabled)}/>
-                    </Grid.Column>  
-                      <Grid.Column width={8}>
-                      <Button basic color='green' onClick={() => onHandleTrackerSelection(undefined)}>OK</Button>
-                      </Grid.Column>
-                    </Grid>
-                  </Card.Content>
-
-                </Card>
-     </Card.Group>
-                </div>)
-                :(undefined)}
-              </Dimmer>
-
-
-
               <Table selectable sortable basic="very" className="tracker-table">
                 <Table.Header>
                   <Table.Row>
@@ -245,9 +164,11 @@ class TrackerDashboard extends Component {
                     ))}
                 </Table.Body>
               </Table>
-            </Dimmer.Dimmable>
           </Grid.Column>
         </Grid>
+        <Segment>
+          <TrackerDetails/>
+        </Segment>
       </div>
     );
   }
